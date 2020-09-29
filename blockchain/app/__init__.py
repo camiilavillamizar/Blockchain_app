495df06b68b68f8717ccf497cfb2155ecd1bd357
Cami V <camiilavillamizar@gmail.com>
2020-09-29 15:51:52 -0500

    Db problem

--- blockchain/app/__init__.py
+++ blockchain/app/__init__.py
@@ -1,5 +1,43 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from app import views
 
 app = Flask(__name__)
 
-from app import views
+db = SQLAlchemy(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
+
+class User(db.Model):
+    user_name: str
+    name: str
+    password : str
+    ip: str
+    email: str
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_name = db.Column(db.String(180), index=False, unique=False, nullable=True)
+    name = db.Column(db.String(180), index=False, unique=False, nullable=True)
+    password = db.Column(db.ARRAY(db.String(180), index=False, unique=False, nullable=True)
+    ip = db.Column(db.String(180), index=False, unique=False, nullable=False)
+    email = db.Column(db.String(180), index=False, unique=False, nullable=True)
+
+    def __init__(self, email, name, password, ip, user_name):
+        self.email = email
+        self.password = password
+        self.ip = ip
+        self.user_name = user_name
+
+    def encrypt_password(self, password):
+        return password
+
+    def serialize(self):
+        return {
+            'user_name': self.user_name,
+            'name': self.name,
+            'ip': self.ip,
+            'email': self.email
+        }
+
+db.init_app(app)
+db.create_all()
--- node_server/models/__init__.py
+++ node_server/models/__init__.py
@@ -5,37 +5,6 @@ import jsons
 from node_server import db
 
 
-class User(db.Model):
-    user_name: str
-    name: str
-    password : str
-    ip: str
-    email: str
-
-    id = db.Column(db.Integer, primary_key=True)
-    user_name = db.Column(db.String(180), index=False, unique=False, nullable=True)
-    name = db.Column(db.String(180), index=False, unique=False, nullable=True)
-    password = db.Column(db.String(180), index=False, unique=False, nullable=True)
-    ip = db.Column(db.String(180), index=False, unique=False, nullable=False)
-    email = db.Column(db.String(180), index=False, unique=False, nullable=True)
-
-    def __init__(self, email, name, password, ip, user_name):
-        self.email = email
-        self.password = self.encrypt_password(password)
-        self.ip = ip
-        self.user_name = user_name
-
-    def encrypt_password(self, password):
-        return password
-
-    def serialize(self):
-        return {
-            'user_name': self.user_name,
-            'name': self.name,
-            'ip': self.ip,
-            'email': self.email
-        }
-
 class Content(db.Model):
     id: int
     name: str
